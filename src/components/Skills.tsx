baa4d58bedbee70603fea10993428bb87a6adae3
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-06-07 00:46:22 -0700

    Increasing size of icons and swapping a few out

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -14,9 +14,9 @@ import {
   faJava,
   faJsSquare,
 } from "@fortawesome/free-brands-svg-icons";
-import { faEnvira } from "@fortawesome/free-brands-svg-icons"; // Replacing faLeaf
-import { faCode } from "@fortawesome/free-solid-svg-icons"; // Placeholder for faCSharp
-// TODO: Word smith skills descriptions
+import { faEnvira } from "@fortawesome/free-brands-svg-icons";
+import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+
 const Skills = () => {
   return (
     <section
@@ -38,96 +38,97 @@ const Skills = () => {
         <div className="grid items-start gap-8 mx-auto sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">React</h3>
-            <FontAwesomeIcon icon={faReact} size="2x" />
+            <FontAwesomeIcon icon={faReact} size="3x" />
             <p className="text-sm">
-              Adept at crafting dynamic, single-page applications with React.
+              Senior level React developer with experience in building complex
+              web applcations.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">TypeScript</h3>
-            <FontAwesomeIcon icon={faJsSquare} size="2x" />
+            <FontAwesomeIcon icon={faJsSquare} size="3x" />
             <p className="text-sm">
-              Skilled in enhancing JavaScript with strong typing using
-              TypeScript.
+              Proficient in writing clean and maintainable code with TypeScript.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">CSS</h3>
-            <FontAwesomeIcon icon={faCss3Alt} size="2x" />
+            <FontAwesomeIcon icon={faCss3Alt} size="3x" />
             <p className="text-sm">
-              Expert in styling web applications with modern CSS techniques.
+              Expert in styling with modern CSS techniques such as responsive
+              design, theming, and tailwind.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">HTML</h3>
-            <FontAwesomeIcon icon={faHtml5} size="2x" />
+            <FontAwesomeIcon icon={faHtml5} size="3x" />
             <p className="text-sm">
-              Proficient in structuring web content with semantic HTML.
+              Understands the fundamentals of HTML and semantic markup.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">Node.js</h3>
-            <FontAwesomeIcon icon={faNodeJs} size="2x" />
+            <FontAwesomeIcon icon={faNodeJs} size="3x" />
             <p className="text-sm">
-              Experienced in building fast and scalable server-side applications
-              with Node.js.
+              Experienced in standing up quick, fast and scalable server-side
+              applications with Node.js.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">C#</h3>
-            <FontAwesomeIcon icon={faCode} size="2x" />
+            <FontAwesomeIcon icon={faLaptopCode} size="3x" />
             <p className="text-sm">
-              Proficient in using .NET Entity Framework for efficient data
-              modeling and database interaction.
+              Capable of using .NET Entity Framework for efficient data modeling
+              and database interaction.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">Java</h3>
-            <FontAwesomeIcon icon={faJava} size="2x" />
+            <FontAwesomeIcon icon={faJava} size="3x" />
             <p className="text-sm">
-              Skilled in creating reliable and high-performance applications
-              with Java.
+              Skilled in creating and maintaining reliable servers with Java.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">Python</h3>
-            <FontAwesomeIcon icon={faPython} size="2x" />
+            <FontAwesomeIcon icon={faPython} size="3x" />
             <p className="text-sm">
               Versatile in writing efficient Python code for a variety of
-              projects.
+              projects such as data analysis, web scraping, and automation.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">SQL</h3>
-            <FontAwesomeIcon icon={faDatabase} size="2x" />
+            <FontAwesomeIcon icon={faDatabase} size="3x" />
             <p className="text-sm">
-              Proficient in crafting complex SQL queries and managing relational
-              databases.
+              Knowledgable in crafting complex SQL queries and managing
+              relational databases.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">MongoDB</h3>
-            <FontAwesomeIcon icon={faEnvira} size="2x" />
+            <FontAwesomeIcon icon={faEnvira} size="3x" />
             <p className="text-sm">
-              Experienced in designing NoSQL databases with MongoDB.
+              Experienced using NoSQL databases with MongoDB for scalable and
+              fast data storage.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">Azure / AWS</h3>
             <div className="flex gap-2">
-              <FontAwesomeIcon icon={faMicrosoft} size="2x" />
-              <FontAwesomeIcon icon={faAws} size="2x" />
+              <FontAwesomeIcon icon={faMicrosoft} size="3x" />
+              <FontAwesomeIcon icon={faAws} size="3x" />
             </div>
             <p className="text-sm">
-              Skilled in deploying and managing applications on Azure and AWS
-              cloud platforms.
+              Familiarity using Azure and AWS platforms for devops and cloud
+              computing.
             </p>
           </div>
           <div className="grid gap-1">
             <h3 className="text-lg font-bold">Git</h3>
-            <FontAwesomeIcon icon={faGitAlt} size="2x" />
+            <FontAwesomeIcon icon={faGitAlt} size="3x" />
             <p className="text-sm">
-              Proficient in version control and team collaboration using Git.
+              Excellent version control and team collaboration using Git.
             </p>
           </div>
         </div>
