841f3b9883bd1b80195f4f93b408fc3b0f8d2d60
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2025-04-24 09:48:09 -0700

    Update project showcase

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -17,33 +17,32 @@ const Projects = () => {
         </div>
         <div className="grid items-start gap-8 mx-auto sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
           <ProjectCard
-            title="Shadowrun FPS"
-            description="Created a web application that allowed users to create and join ranked games of Shadowrun, an old school FPS game."
+            title="Housing Market Trends"
+            description="Created a web application that allows users to view and analyze housing market trends in the US."
             technologies={[
-              "Next.js",
-              "MongoDB",
+              "React",
+              "HeadlessUI",
+              "Recharts",
+              "D3",
+              "React-Query",
               "TailwindCSS",
-              "ShadcnUI",
-              "NextAuth",
             ]}
-            liveLink="https://www.shadowrunfps.com/"
-            githubLinks={["https://github.com/Shadowrun-FPS/shadowrun-fps"]}
+            liveLink="https://housing-market-trends.netlify.app/"
+            githubLinks={["https://github.com/tmorse01/housing-visualizations"]}
           />
           <ProjectCard
-            title="Formbox"
-            description="Created an application that enabled users to build dynamic forms, share a link to collect responses, then view responses."
+            title="NW Builds"
+            description="Created a web application to showcase builds from one of my favorite online MMORPG games, New World."
             technologies={[
               "React",
               "Node.js",
-              "MongoDB",
-              "React Router",
-              "Refresh Token Auth",
+              "Express.js",
+              "Mantine UI",
+              "MongoDB(Mongoose)",
+              "Cloudinary",
             ]}
-            liveLink="https://formboxapp.com/"
-            githubLinks={[
-              "https://github.com/tmorse01/formbox",
-              "https://github.com/tmorse01/formboxserver",
-            ]}
+            liveLink="https://nw-builds.netlify.app/"
+            githubLinks={["https://github.com/tmorse01/nw-builds"]}
           />
           <ProjectCard
             title="Mob Unity"
