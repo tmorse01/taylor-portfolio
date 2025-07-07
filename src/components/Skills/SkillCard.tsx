e21d56699fb60351a112bf09df6e22a7afbdf59b
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2025-07-06 18:09:30 -0700

    Refactor SkillCard component to conditionally apply height based on expanded state

--- src/components/Skills/SkillCard.tsx
+++ src/components/Skills/SkillCard.tsx
@@ -21,7 +21,11 @@ const SkillCard: React.FC<SkillCardProps> = ({ title, icon, description }) => {
     : description;
 
   return (
-    <div className="flex flex-col items-center w-full p-6 text-center transition-all duration-300 transform border rounded-lg shadow-lg bg-blue-600/20 backdrop-blur-md border-blue-300/20 hover:scale-105 hover:-translate-y-2 h-72">
+    <div
+      className={`flex flex-col items-center w-full p-6 text-center transition-all duration-300 transform border rounded-lg shadow-lg bg-blue-600/20 backdrop-blur-md border-blue-300/20 hover:scale-105 hover:-translate-y-2 ${
+        isExpanded ? "" : "h-72"
+      }`}
+    >
       <div className="mb-4 text-5xl text-cyan-400">{icon}</div>
       <h3 className="mb-2 text-xl font-bold text-white">{title}</h3>
       <div
